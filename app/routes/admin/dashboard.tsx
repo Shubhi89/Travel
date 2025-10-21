b6111860a1862ecc65a375b82308812f6e6a8bd1
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-10-21 11:56:04 +0530

    trips card

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,51 +1,60 @@
-import { Header, StatsCard, TripCard } from 'components';
-import React from 'react'
+import { Header, StatsCard, TripCard } from "components";
+import React from "react";
+import { dashboardStats, user, allTrips } from "~/constants";
+
+const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
 
 const Dashboard = () => {
-  const user = {name: 'Shubhi'};
-  const dashboardStats = {
-    totalUsers: 12450,
-    usersJoined : {currentMonth : 218 , lastMonth :176},
-    totalTrips : 3210,
-    tripsCreated : {currMonth : 150 , lastMonth : 250},
-    userRole : {total : 62 , currentMonth : 25 , lastMonth: 15},
-  }
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome , ${user.name}`}
+        description={
+          "Track activity , trends and popular destinations in real time"
+        }
+      />
 
-  const {totalUsers , usersJoined , totalTrips , tripsCreated , userRole} = dashboardStats;
-  return(
-  <main className='dashboard wrapper'>
-    <Header title={`Welcome`}
-    description={'Track activity , trends and popular destinations in real time'}/>
-
-    <section className='flex flex-col gap-6'>
-      <div className='grid grid-cols-1 md: grid-cols-3 gap-6 w-full'>
-
-      </div>
-    </section>
-    <StatsCard
-      headerTitle='total users'
-      total={totalUsers}
-      currentMonthCount={usersJoined.currentMonth}
-      lastMonthCount={usersJoined.lastMonth}
-
-    />
-    <StatsCard
-      headerTitle='total trips'
-      total={totalTrips}
-      currentMonthCount={tripsCreated.currMonth}
-      lastMonthCount={tripsCreated.lastMonth}
-
-    />
-    <StatsCard
-      headerTitle='users'
-      total={userRole.total}
-      currentMonthCount={userRole.currentMonth}
-      lastMonthCount={userRole.lastMonth}
-
-    />
-    <TripCard/>
-  </main>
-  )
-}
+      <section className="flex flex-col gap-6">
+        <div className="lg:grid grid-cols-3 md: grid-cols-1 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total Users"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Total Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Active Users"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <section className="container">
+        <h1 className="text-xl font-semibold text-dark-100">
+          Created Trips
+        </h1>
+        <div className="trip-grid">
+          {allTrips.slice(0,4).map(({id , name , imageUrls , itinerary, tags , estimatedPrice})=> (
+            <TripCard key={id}
+            id={id.toString()}
+            name={name}
+            imageUrl={imageUrls[0]}
+            location={itinerary?.[0]?.location ?? ''}
+            tags={tags}
+            price={estimatedPrice}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Dashboard;
