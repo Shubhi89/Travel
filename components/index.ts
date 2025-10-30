f271a9f1d63216bd462567b0e28bbff974e7c416
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-10-30 10:31:34 +0530

    resetting

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export {default as NavItems } from './NavItems';
-export {default as MobileSideBar} from './MobileSidebar';
-export {default as Header} from './Header';
-export {default as StatsCard} from './StatsCard';
-export {default as TripCard} from './TripCard';
