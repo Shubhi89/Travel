4aa3c6b644cfa12bcb22b58a999fa52bca89fd1c
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-11-05 10:18:57 +0530

    auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Layout = ({children}:{children: React.ReactNode}) => {
   return (
-    <main className='min-h-screen text-gray-400'>
+    <main className='auth-layout'>
+        <section className='auth-left-section scrollbar-hide-default'>
+            <Link href='/' className='auth-logo flex items-center space-x-2'>
+                <Image src={'/favicon.ico'} alt='Logo' width={140} height={32} className='h-8 w-auto cursor-pointer'/>
+                <span className='font-bold text-2xl text-white'>TradeVerse</span>
+            </Link>
+            <div className='pb-6 lg:pb-8 flex-1'>{children}</div>
+        </section>
+        <section className='auth-right-section'>
+            <div className='z-10 relative lg:mt-4 lg:mb-16'>
+                <blockquote className='auth-blockquote'>
+                    TradeVerse turned my trading experience around. The intuitive interface and real-time data helped me make informed decisions and boost my profits!
+                </blockquote>
+                <div className='flex items-center justify-between'>
+                    <div>
+                        <cite className='auth-testimonial-author'>- John Doe</cite>
+                        <p className='max-md:text-xs text-gray-500'>Retail Investor</p>
+                    </div>
+                    <div className='flex items-center gap-0.5'>
+                        {[1,2,3,4,5].map((star) => (
+                            <Image src={'/assets/icons/star.svg'} alt='star' key={star} height={20} width={20} className='w-5 h-5'/>
+                        ))}
+                    </div>
+                </div>
+            </div>
 
+            <div className='flex-1 relative'>
+                <Image src={'/assets/images/dashboard.png'} alt='Dashboard' width={1440} height={1150} className='auth-dashboard-preview absolute top-0'/>
+            </div>
+        </section>
     </main>
   )
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+'use client'
+import { Button } from '@/components/ui/button';
+import { useForm } from 'react-hook-form';
 
 const SignUpPage = () => {
+    const {
+        register , handleSubmit , control , formState:{errors , isSubmitting},
+    } = useForm<SignUpFormData>({
+        defaultValues : {
+            fullName: '',
+            email: '',
+            password: '',
+            country: 'India',
+            investmentGoals: 'Growth',
+            riskTolerance: 'Medium',
+            preferredIndustry: 'Technology',
+        },
+        mode: 'onBlur'
+    });
+    const onSubmit = async(data : SignUpFormData) => {
+        try{
+            console.log(data);
+        } catch(e) {
+            console.error(e);
+        }
+    };
   return (
-    <div>SignUpPage</div>
+    <>
+       <h1 className='form-title'>Sign Up & Personalize</h1>
+       <form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+        
+        <Button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5'>
+            {isSubmitting ? 'Creating Account' : 'Start Your Investing Journey'}
+        </Button>
+       </form>
+    </>
   )
 }
 
